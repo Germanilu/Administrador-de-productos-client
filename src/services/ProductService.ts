32036cb844bdcd1c3e555088d71fe64ba27782ee
Luciano Germani <lucianogermani93@gmail.com>
2025-05-27 12:48:57 +0200

    Feat: Create reusable componente for form inputs

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,6 +1,7 @@
 import { safeParse } from 'valibot'
 import axios from 'axios'
 import {DraftProductSchema, ProductsSchema, ProductSchema, type Product} from '../types'
+import { toBoolean } from '../utils'
 
 type ProductData = {
     [k: string]: FormDataEntryValue
@@ -52,6 +53,24 @@ export async function getProductsById(id:Product['id']){
             throw new Error('Hubo un error...')
          }
          
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function updateProduct(data : ProductData, id: Product['id']) {
+    try {
+        const result = safeParse(ProductSchema, {
+            id,
+            name:data.name,
+            price: +data.price,
+            availability: toBoolean(data.availability.toString())
+        })
+        if(result.success){
+            const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`
+            const {data} = await axios.put(url,result.output)
+        }
+
     } catch (error) {
         console.log(error)
     }
--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -1,38 +1,45 @@
 import { Link, Form, redirect, useActionData, type ActionFunctionArgs, type LoaderFunctionArgs, useLoaderData } from "react-router-dom"
 import ErrorMessage from "../components/ErrorMessage"
-import { addProduct, getProductsById } from "../services/ProductService"
+import { addProduct, getProductsById, updateProduct } from "../services/ProductService"
 import type { Product } from "../types"
+import ProductForm from "../components/ProductForm"
 
-export async function loader ({params}: LoaderFunctionArgs){
-console.log(params)
-if(params.id !== undefined){
-    const product = await getProductsById(+params.id)
-    if(!product){
-        return redirect('/')
+export async function loader({ params }: LoaderFunctionArgs) {
+    if (params.id !== undefined) {
+        const product = await getProductsById(+params.id)
+        if (!product) {
+            return redirect('/')
+        }
+        return product
     }
-    return product
-}
 }
 
 // Action es una funcion de react-router-dom donde proceso todos los datos del formulario. Esta funcion siempre retorna algo
-export async function action({request}: ActionFunctionArgs) {
+export async function action({ request, params }: ActionFunctionArgs) {
     const data = Object.fromEntries(await request.formData())
     let error = ''
-    if(Object.values(data).includes('')){
+    if (Object.values(data).includes('')) {
         error = 'Todos los campos son obligatorios'
     }
-    if(error.length){
+    if (error.length) {
         return error
     }
-    await addProduct(data)
-    //Redirecciona a homepage cuando acaba de hacer el post 
-    return redirect('/')
+    if (params.id !== undefined) {
+        await updateProduct(data, +params.id)
+        //Redirecciona a homepage cuando acaba de hacer el post 
+        return redirect('/')
+    }
 }
 
+
+const availabilityOptions = [
+    { name: 'Disponible', value: true },
+    { name: 'No Disponible', value: false }
+]
+
 export default function EditProduct() {
-
+    //Recupero el Producto seleccionado con useLoaderData de react-router-dom
     const product = useLoaderData() as Product
-
     //Este es un hook que permite recuperar el error del formulario ya que en principio no esta conectado con el componente al procesarse en la funcion action() de arriba
     const error = useActionData() as string
 
@@ -56,40 +63,27 @@ export default function EditProduct() {
                 className="mt-10"
                 method='POST'
             >
+                <ProductForm product={product}/>
                 <div className="mb-4">
                     <label
                         className="text-gray-800"
-                        htmlFor="name"
-                    >Nombre Producto:</label>
-                    <input
-                        id="name"
-                        type="text"
+                        htmlFor="availability"
+                    >Disponibilidad:</label>
+                    <select
+                        id="availability"
                         className="mt-2 block w-full p-3 bg-gray-50"
-                        placeholder="Nombre del Producto"
-                        name="name"
-                        defaultValue={product.name}
-
-                    />
-                </div>
-                <div className="mb-4">
-                    <label
-                        className="text-gray-800"
-                        htmlFor="price"
-                    >Precio:</label>
-                    <input
-                        id="price"
-                        type="number"
-                        className="mt-2 block w-full p-3 bg-gray-50"
-                        placeholder="Precio Producto. ej. 200, 300"
-                        name="price"
-                         defaultValue={product.price}
-
-                    />
+                        name="availability"
+                        defaultValue={product?.availability.toString()}
+                    >
+                        {availabilityOptions.map(option => (
+                            <option key={option.name} value={option.value.toString()}>{option.name}</option>
+                        ))}
+                    </select>
                 </div>
                 <input
                     type="submit"
                     className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
-                    value="Editar Producto"
+                    value="Guardar Cambios"
                 />
             </Form>
         </>
